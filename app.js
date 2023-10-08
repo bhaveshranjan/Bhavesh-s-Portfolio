e09c2806433d743620efc21e88d1c183ab188457
Bhavesh Ranjan <75161205+bhaveshranjan@users.noreply.github.com>
2023-10-08 15:31:51 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-(function () {
+/*(function () {
     [...document.querySelectorAll(".control")].forEach(button => {
         button.addEventListener("click", function() {
             document.querySelector(".active-btn").classList.remove("active-btn");
@@ -11,3 +11,42 @@
         document.body.classList.toggle("light-mode");
     })
 })();
+
+*/
+
+
+
+(function () {
+    // Function to smoothly scroll to a target element
+    function smoothScroll(target) {
+        const targetElement = document.getElementById(target);
+        if (targetElement) {
+            window.scrollTo({
+                top: targetElement.offsetTop,
+                behavior: "smooth"
+            });
+        }
+    }
+
+    // Event listeners for navigation buttons
+    [...document.querySelectorAll(".control")].forEach(button => {
+        button.addEventListener("click", function() {
+            document.querySelector(".active-btn").classList.remove("active-btn");
+            this.classList.add("active-btn");
+            document.querySelector(".active").classList.remove("active");
+            const target = button.dataset.id;
+            document.getElementById(target).classList.add("active");
+            
+            // Smoothly scroll to the clicked section
+            smoothScroll(target);
+        })
+    });
+
+    // Toggle light/dark mode
+    document.querySelector(".theme-btn").addEventListener("click", () => {
+        document.body.classList.toggle("light-mode");
+    })
+})();
+
+
+
